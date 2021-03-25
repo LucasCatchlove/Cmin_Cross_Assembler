bd82d07ec754d5b380ecbda9e0ed2772d3483276
Lucas Catchlove <lucascatchlove@gmail.com>
2021-03-25 15:56:33 -0400

    finished refactoring tests and adding new scanner and parser tests

--- tests/TestInstruction.java
+++ tests/TestInstruction.java
@@ -2,20 +2,23 @@ package tests;
 
 import components.Instruction;
 import components.Mnemonic;
+import components.MnemonicType;
 
 public class TestInstruction {
 
     public static void main(String[] args) {
 
-//        Instruction inst1 = new Instruction(new Mnemonic("teq", 0x1A), "");
-//        Instruction inst2 = new Instruction(new Mnemonic("add", 0x0C), "");
-//        Instruction inst3 = new Instruction(new Mnemonic("sub", 0x0D), "");
-//
-//        System.out.println("Test components.Instruction getMnemonic");
-//        System.out.println("Name: teq, Opcode: 1A Name: add, Opcode: 0C Name: sub, Opcode: 0D");
-//        System.out.print(inst1.getMnemonic()+" ");
-//        System.out.print(inst2.getMnemonic()+ " ");
-//        System.out.println(inst3.getMnemonic());
+
+        Instruction inst1 = new Instruction(new Mnemonic("teq", 0x1A, MnemonicType.Inherent), "");
+        Instruction inst2 = new Instruction(new Mnemonic("add", 0x0C, MnemonicType.Inherent), "");
+        Instruction inst3 = new Instruction(new Mnemonic("sub", 0x0D, MnemonicType.Inherent), "");
+
+        System.out.println("Test components.Instruction getMnemonic");
+        System.out.println("Name: teq, Opcode: 1A Name: add, Opcode: 0C Name: sub, Opcode: 0D");
+        System.out.print(inst1.getMnemonic()+" ");
+        System.out.print(inst2.getMnemonic()+ " ");
+        System.out.println(inst3.getMnemonic());
+
 
     }
 }
